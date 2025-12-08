4ef56600d5ae23e7ec9f00ee99f8b1b639aa5a4f
desmondvincent <desmond.j.vincent@gmail.com>
2025-12-08 11:10:56 -0600

    feat: Add sliding-window project

---
+++ Sliding-Window/src/main/java/org/algorithms/Main.java
@@ -0,0 +1,10 @@
+package org.algorithms;
+
+public class Main {
+    static void main() {
+
+        int [] arr = {1,2,3,4,5,6};
+        int k = 3;
+        Window window = new Window(arr, k);
+    }
+}
