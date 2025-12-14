a5f312c1222d4fc77a4306b09b70664320847c9d
desmondvincent <desmond.j.vincent@gmail.com>
2025-12-14 14:27:36 -0600

    feat: Add listnode class to handle the connections of the tree

---
+++ Breadth-First-Search/src/main/java/org/algorithms/ListNode.java
@@ -0,0 +1,13 @@
+package org.algorithms;
+
+public class ListNode {
+    public int val;
+    public ListNode left;
+    public ListNode right;
+
+    public ListNode(int val) {
+        this.val = val;
+        left = right = null;
+    }
+
+}
