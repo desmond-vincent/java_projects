6393f4752546107709e6ff8cb1b6feb86a935a4a
desmondvincent <desmond.j.vincent@gmail.com>
2026-01-01 23:14:38 -0600

    feat: Add library-system project, and structure app

---
+++ Library-System/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    static String intro = "======= LIBRARY SYSTEM =========";
+    static String option = "\n1. Register\n2. Login\n3. Exit";
+
+    static void main() {
+        IO.println(intro + option);
+    }
+}
